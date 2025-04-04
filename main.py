3ee6f7b52a1b9521a1da3e130b26d6908faf8858
Anderson Gomes <andersonrmgomes@msn.com>
2025-04-04 16:09:37 -0300

    updates

--- main.py
+++ main.py
@@ -57,24 +57,40 @@ frame_direito = ctk.CTkFrame(app, width=350, height=400)
 frame_esquerdo.pack(side="left", fill="both", expand=True, padx=5, pady=5)
 frame_direito.pack(side="right", fill="both", expand=True, padx=5, pady=5)
 
-label_titulo = ctk.CTkLabel(frame_esquerdo, text="Conversor de Temperatura", font=("Arial", 20), anchor="center")
-label_titulo.pack(pady=10, padx=20, anchor="center")
-largura_input = 220
-entrada_temp = ctk.CTkEntry(frame_esquerdo, placeholder_text="Digite a temperatura", width=largura_input)
-resultado = StringVar(value="Resultado")
-menu_opcoes = ctk.CTkComboBox(frame_esquerdo, values=["Celsius para Fahrenheit", "Fahrenheit para Celsius"], state="readonly", width=largura_input)
+# Frame interno centralizador
+frame_conteudo_esquerdo = ctk.CTkFrame(frame_esquerdo, fg_color="transparent")
+frame_conteudo_esquerdo.place(relx=0.5, rely=0.0, anchor="n")
 
-# Define uma opção padrão no ComboBox
+
+largura_input = 220
+entrada_temp = ctk.CTkEntry(
+    frame_conteudo_esquerdo,
+    placeholder_text="Digite a temperatura",
+    width=largura_input,
+    justify="center"
+)
+
+resultado = StringVar(value="Resultado")
+
+menu_opcoes = ctk.CTkComboBox(
+    frame_conteudo_esquerdo,
+    values=["Celsius para Fahrenheit", "Fahrenheit para Celsius"],
+    state="readonly",
+    width=largura_input,
+    justify="center"
+)
 menu_opcoes.set("Celsius para Fahrenheit")
 
-entrada_temp.pack(pady=10, padx=20, anchor="w")
-menu_opcoes.pack(pady=5, padx=20, anchor="w")
-ctk.CTkButton(frame_esquerdo, text="Converter", command=converter_temperatura).pack(pady=20, padx=20, anchor="w")
+label_titulo = ctk.CTkLabel(frame_conteudo_esquerdo, text="Conversor de Temperatura", font=("Arial", 20))
+label_titulo.pack(pady=10)
+entrada_temp.pack(pady=10)
+menu_opcoes.pack(pady=5)
+ctk.CTkButton(frame_conteudo_esquerdo, text="Converter", command=converter_temperatura).pack(pady=20)
 
 botao_tema = ctk.CTkButton(frame_esquerdo, text="⚙", command=alternar_tema, width=40, height=40, font=("Segoe UI Symbol", 20))
-botao_tema.place(x=10, y=345)
+botao_tema.place(x=10, y=330)
 
-label_resultado = ctk.CTkLabel(frame_direito, textvariable=resultado, font=("Arial", 45), anchor="center", width=300, height=200)
+label_resultado = ctk.CTkLabel(frame_direito, textvariable=resultado, font=("Arial", 45), anchor="center", width=350, height=200)
 label_resultado.place(relx=0.5, rely=0.5, anchor="center")
 label_resultado.pack(expand=True)
 
